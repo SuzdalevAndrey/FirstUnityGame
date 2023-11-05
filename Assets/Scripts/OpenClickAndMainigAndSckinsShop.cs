a670b65288253ed32804dc99d089933bc4c82f60
Andrey Suzdalev <suzdalev2005@mail.ru>
2023-11-05 20:35:19 +0300

    Чуть-чуть поменял дизайн

--- Assets/Scripts/OpenClickAndMainigAndSckinsShop.cs
+++ Assets/Scripts/OpenClickAndMainigAndSckinsShop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,9 +9,15 @@ public class OpenClickShop : MonoBehaviour
     public GameObject mainShop;
     public GameObject childShop1;
     public GameObject childShop2;
+
+    public GameObject button1;
+    public GameObject button2;
     public void OnClickOpenShop(){
         mainShop.SetActive(true);
         childShop1.SetActive(false);
         childShop2.SetActive(false);
+        gameObject.GetComponent<Image>().color=Color.green;
+        button1.GetComponent<Image>().color=Color.white;
+        button2.GetComponent<Image>().color=Color.white;
     }
 }
